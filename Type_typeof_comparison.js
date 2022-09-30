a925a2087374a6aaff6e0c592cf9785c1c23dad5
GangaKadam <112053570+GangaKadam@users.noreply.github.com>
2022-09-30 14:55:36 +0530

    firstcommit1

---
+++ Type_typeof_comparison.js
@@ -0,0 +1,20 @@
+//type
+
+let x1 = 10; //It will store nubmer type value like 10 ,-10, 10.45
+
+let x2 = "Hello"; //collection of cherecter
+
+let x3 = true; // true or false
+
+//typeof operator ---- if we want to know type of veriable
+
+console.log(typeof x1);
+console.log(typeof x2);
+console.log(typeof x3);
+
+//comparison operator
+// > , < , >= ,<= , == ,!= ,=== ,!==
+
+console.log(3 > 4);
+console.log(4 == "4");
+console.log(4 === 4);
